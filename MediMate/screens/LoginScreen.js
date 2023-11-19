bae604168eacada69d991e70945e18de384c32e4
xhans <hanwinhtun334@gmail.com>
2023-11-19 10:27:35 -0800

    styles and med screen

--- MediMate/screens/LoginScreen.js
+++ MediMate/screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { View, Text, TextInput, Button } from  'react-native';
 
+
 const LoginScreen = ({ navigation }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -10,21 +11,62 @@ const LoginScreen = ({ navigation }) => {
     };
 
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            {/* Logo image */}
+            <Image
+                source={require('./path-to-your-logo.png')}
+                style={styles.logo}
+                resizeMode="contain"
+            />
+
+            <Text style={styles.title}>Login</Text>
+
             <TextInput
+                style={styles.input}
                 placeholder="Username"
                 value={username}
                 onChangeText={(text) => setUsername(text)}
             />
+
             <TextInput
+                style={styles.input}
                 placeholder="Password"
                 value={password}
                 onChangeText={(text) => setPassword(text)}
+                secureTextEntry
             />
+
             <Button title="Login" onPress={handleLogin} />
+
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 16,
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        marginBottom: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+    input: {
+        height: 40,
+        width: '100%',
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 12,
+        paddingHorizontal: 10,
+    },
+});
 
 export default LoginScreen;
